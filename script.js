62c1d7d07a2535cd9ce758041d6a99cff9c943e5
mashot7 <mashot2@gmail.com>
2020-07-31 11:18:54 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/* construct manually */
+let bar1 = new ldBar("#myItem1");
+/* ldBar stored in the element */
+// let bar2 = document.getElementById("myItem1").ldBar;
+bar1.set(46);
+
+/* construct manually */
+let bar2 = new ldBar("#myItem2");
+/* ldBar stored in the element */
+// let bar2 = document.getElementById("myItem1").ldBar;
+bar2.set(74);
+
+/* construct manually */
+let bar3 = new ldBar("#myItem3");
+/* ldBar stored in the element */
+// let bar2 = document.getElementById("myItem1").ldBar;
+bar3.set(14);
